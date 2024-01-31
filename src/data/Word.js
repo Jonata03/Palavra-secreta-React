d19e51a9da1466a6ca2021d3e7b533b858199e9e
Jonata <jonatanery2016@gmail.com>
2024-01-30 22:10:53 -0400

    Subindo o projeto da palavra secreta

---
+++ src/data/Word.js
@@ -0,0 +1,8 @@
+export const wordslist ={
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "cérebro", "pescoço", "olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "Javascript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "0vo"],
+};
